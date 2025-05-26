6151981b778c1cf1426946660382670cb563b15e
Razvan <razvan@razshare.dev>
2025-05-26 03:33:09 +0200

    chore(app): bump.

--- lib/sessions/data.go
+++ lib/sessions/data.go
@@ -3,27 +3,14 @@ package sessions
 import "time"
 
 type SessionData struct {
-	Items        []Item    `json:"items"`
 	LastActivity time.Time `json:"lastActivity"`
 	Verified     bool      `json:"verified"`
 	Expired      bool      `json:"expired"`
 	AccountId    string    `json:"accountId"`
 }
 
-type Item struct {
-	Checked     bool   `json:"checked"`
-	Description string `json:"description"`
-}
-
 func NewSessionData() SessionData {
 	return SessionData{
-		Items: []Item{
-			{Checked: false, Description: "Pet the cat."},
-			{Checked: false, Description: "Do laundry"},
-			{Checked: false, Description: "Pet the cat."},
-			{Checked: false, Description: "Cook"},
-			{Checked: false, Description: "Pet the cat."},
-		},
 		LastActivity: time.Now(),
 		Expired:      false,
 		Verified:     true,
